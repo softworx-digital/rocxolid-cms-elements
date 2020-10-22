a3786b832b2b6c26d0293bf4b14acdd9a0e047d4
vagrant <vagrant@homestead-softworx>
2020-10-22 21:43:59 +0000

    general update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -66,7 +66,7 @@ class ServiceProvider extends RocXolidAbstractServiceProvider
         // php artisan vendor:publish --provider="Softworx\RocXolid\CMS\Elements\ServiceProvider" --tag="lang" (--force to overwrite)
         $this->publishes([
             //__DIR__ . '/../resources/lang' => resource_path('lang/vendor/softworx/rocXolid/cms-elements'),
-            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/rocXolid:cms-elements'),
+            __DIR__ . '/../resources/lang' => resource_path('lang/vendor/rocXolid:cms-elements'), // used by laravel's FileLoaded::loadNamespaceOverrides()
         ], 'lang');
 
         // views files
